b7bdd730fde178904101930ae0e4361ed7f3be04
Trey Baxter <treywbaxter@gmail.com>
2025-03-25 23:51:56 -0700

    Initial config

--- macos/.config/sketchybar/plugins/app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FOCUSED_APP=$(osascript -e 'tell application "System Events" to get name of first application process whose frontmost is true')
-
-if [[ $FOCUSED_APP = "" ]]; then
-  FOCUSED_APP="Desktop"
-fi
-
-sketchybar --set $NAME label="$FOCUSED_APP"
--- macos/.config/sketchybar/plugins/cpu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sketchybar --set "$NAME" label="$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{ printf "%.0f%%", $3 + $5"%" }')"
